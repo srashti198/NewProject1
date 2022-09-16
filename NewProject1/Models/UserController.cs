79511b1d25f50ae31b3ee62ac1f7d98f91584e56
admin <admin@DESKTOP-HJN028O>
2022-09-16 11:48:08 +0530

    Add project files.

---
+++ NewProject1/Models/UserController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewProject1.Models
+{
+    public class UserController
+    {
+        public int Number { get; set; }
+        public string username { get; set; }
+        public string Password { get; set; }
+    }
+}
